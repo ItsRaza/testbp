28ea09e8ac3ee6eb54002eee64ea761759747308
ItsRaza <razaabbas768@gmail.com>
2020-09-11 02:42:13 +0500

    commit, testuser model added

---
+++ raza/backend/app/alembic/versions/36460ed66547_testuser_model_added.py
@@ -0,0 +1,24 @@
+"""testuser model added
+
+Revision ID: 36460ed66547
+Revises: d4867f3a4c0a
+Create Date: 2020-09-11 02:41:36.253076
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '36460ed66547'
+down_revision = 'd4867f3a4c0a'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
