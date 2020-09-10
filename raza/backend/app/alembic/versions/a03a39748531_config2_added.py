7b2fe0661990a9b2053bedb209a2b1e3d3c968fa
ItsRaza <razaabbas768@gmail.com>
2020-09-11 03:15:26 +0500

    commit

---
+++ raza/backend/app/alembic/versions/a03a39748531_config2_added.py
@@ -0,0 +1,24 @@
+"""config2 added
+
+Revision ID: a03a39748531
+Revises: 36460ed66547
+Create Date: 2020-09-11 03:13:16.900116
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a03a39748531'
+down_revision = '36460ed66547'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
